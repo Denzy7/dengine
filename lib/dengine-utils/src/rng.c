de3a22d5b77d11abb44578e8b21d1339875ecbad
Denzy7 <dennismyner7@gmail.com>
2022-01-19 09:30:52 +0300

    Fix glibc dep (rand_s)

--- lib/dengine-utils/src/rng.c
+++ lib/dengine-utils/src/rng.c
@@ -2,11 +2,10 @@
 
 #include <stdlib.h> //rand_r()
 #include <time.h> //time_t, time()
-unsigned int _seed = 0;
 
 void dengineutils_rng_set_seed(unsigned int seed)
 {
-    _seed = seed;
+    srand(seed);
 }
 
 void dengineutils_rng_set_seedwithtime()
@@ -14,10 +13,10 @@ void dengineutils_rng_set_seedwithtime()
     time_t t;
     time(&t);
 
-    _seed = t;
+    srand(t);
 }
 
 int dengineutils_rng_int(int max)
 {
-    return rand_r(&_seed) % max;
+    return rand() % max;
 }
