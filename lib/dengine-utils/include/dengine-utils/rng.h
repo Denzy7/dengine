0ca741a7f577aa8baeee07e38ecf4f861bb59f1b
Denzy7 <dennismyner7@gmail.com>
2022-01-20 16:51:38 +0300

    extern "C" dengine-utils/rng.h

--- lib/dengine-utils/include/dengine-utils/rng.h
+++ lib/dengine-utils/include/dengine-utils/rng.h
@@ -1,9 +1,14 @@
 #ifndef RNG_H
 #define RNG_H
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 int dengineutils_rng_int(int max);
 
 void dengineutils_rng_set_seed(unsigned int seed);
 
 void dengineutils_rng_set_seedwithtime();
+#ifdef __cplusplus
+}
+#endif
 #endif // RNG_H
