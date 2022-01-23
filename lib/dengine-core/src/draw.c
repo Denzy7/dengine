7b825e643c02508e8826f49cc26969fe5a0f27ce
Denzy7 <dennismyner7@gmail.com>
2022-01-23 16:03:56 +0300

    Check every draw procedure instead

--- lib/dengine-core/src/draw.c
+++ lib/dengine-core/src/draw.c
@@ -7,22 +7,22 @@
 void dengine_draw_primitive(Primitive* primitive, Shader* shader)
 {
     //Don't draw if we have an ERROR. Prevent's infinite spawn of dialogs
-    if(!DENGINE_CHECKGL)
-    {
-        glBindVertexArray(primitive->vao);
+    glBindVertexArray(primitive->vao);
+    DENGINE_CHECKGL;
 
-        dengine_buffer_bind(GL_ARRAY_BUFFER, &primitive->array);
-        dengine_buffer_bind(GL_ELEMENT_ARRAY_BUFFER, &primitive->index);
+    dengine_buffer_bind(GL_ARRAY_BUFFER, &primitive->array);
+    dengine_buffer_bind(GL_ELEMENT_ARRAY_BUFFER, &primitive->index);
 
-        dengine_shader_use(shader);
+    dengine_shader_use(shader);
 
-        glDrawElements(primitive->draw_mode, primitive->index_count, primitive->draw_type, 0);
+    glDrawElements(primitive->draw_mode, primitive->index_count, primitive->draw_type, 0);
+    DENGINE_CHECKGL;
 
-        dengine_buffer_bind(GL_ARRAY_BUFFER, NULL);
-        dengine_buffer_bind(GL_ELEMENT_ARRAY_BUFFER, NULL);
+    dengine_buffer_bind(GL_ARRAY_BUFFER, NULL);
+    dengine_buffer_bind(GL_ELEMENT_ARRAY_BUFFER, NULL);
 
-        dengine_shader_use(NULL);
+    dengine_shader_use(NULL);
 
-        glBindVertexArray(0);
-    }
+    glBindVertexArray(0);
+    DENGINE_CHECKGL;
 }
