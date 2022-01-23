4cda2a8f82013ebf6893027520be1df578560191
Denzy7 <dennismyner7@gmail.com>
2022-01-23 13:59:40 +0300

    Draw only without error

--- lib/dengine-core/src/draw.c
+++ lib/dengine-core/src/draw.c
@@ -3,23 +3,26 @@
 #include "loadgl.h"
 
 #include "buffer.h" //bind
+
 void dengine_draw_primitive(Primitive* primitive, Shader* shader)
 {
-    glBindVertexArray(primitive->vao);
-
-    dengine_buffer_bind(GL_ARRAY_BUFFER, &primitive->array);
-    dengine_buffer_bind(GL_ELEMENT_ARRAY_BUFFER, &primitive->index);
-
-    dengine_shader_use(shader);
+    //Don't draw if we have an ERROR. Prevent's infinite spawn of dialogs
     if(!DENGINE_CHECKGL)
+    {
+        glBindVertexArray(primitive->vao);
+
+        dengine_buffer_bind(GL_ARRAY_BUFFER, &primitive->array);
+        dengine_buffer_bind(GL_ELEMENT_ARRAY_BUFFER, &primitive->index);
+
+        dengine_shader_use(shader);
+
         glDrawElements(primitive->draw_mode, primitive->index_count, primitive->draw_type, 0);
-    //DRAW ERROR WILL CAUSE INFINITE SPAWN OF DIALOGS!
 
+        dengine_buffer_bind(GL_ARRAY_BUFFER, NULL);
+        dengine_buffer_bind(GL_ELEMENT_ARRAY_BUFFER, NULL);
 
-    dengine_buffer_bind(GL_ARRAY_BUFFER, NULL);
-    dengine_buffer_bind(GL_ELEMENT_ARRAY_BUFFER, NULL);
+        dengine_shader_use(NULL);
 
-    dengine_shader_use(NULL);
-
-    glBindVertexArray(0);
+        glBindVertexArray(0);
+    }
 }
