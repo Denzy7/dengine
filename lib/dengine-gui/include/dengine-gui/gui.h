1d6108d5c57f92692fffccffe21800bc1d9d6d29
Denzy7 <dennismyner7@gmail.com>
2022-02-04 12:57:47 +0300

    Add a basic window gui panel

--- lib/dengine-gui/include/dengine-gui/gui.h
+++ lib/dengine-gui/include/dengine-gui/gui.h
@@ -1,9 +1,13 @@
 #ifndef GUI_H
 #define GUI_H
 
+#include "dengine/texture.h"
+
 int denginegui_init();
 
 int denginegui_set_font(void* ttf, const float fontsize, unsigned int bitmap_size);
 
 void denginegui_text(float x,float y, const char* text, float* rgba);
+
+void denginegui_panel(float x,float y, float width, float height, Texture* texture, float* uv, float* rgba);
 #endif // GUI_H
