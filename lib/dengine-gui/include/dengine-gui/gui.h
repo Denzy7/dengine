ec9fed86e48d60ec3353089b54413ab6e3863d90
Denzy7 <dennismyner7@gmail.com>
2022-02-06 20:25:13 +0300

    Add a gui button

--- lib/dengine-gui/include/dengine-gui/gui.h
+++ lib/dengine-gui/include/dengine-gui/gui.h
@@ -10,4 +10,7 @@ int denginegui_set_font(void* ttf, const float fontsize, unsigned int bitmap_siz
 void denginegui_text(float x,float y, const char* text, float* rgba);
 
 void denginegui_panel(float x,float y, float width, float height, Texture* texture, float* uv, float* rgba);
+
+int denginegui_button(float x,float y, float width, float height, const char* text, float* rgba);
+
 #endif // GUI_H
