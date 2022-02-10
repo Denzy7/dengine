a8539adfdd8564311472c141b974823c0843e1b0
Denzy7 <dennismyner7@gmail.com>
2022-02-10 14:25:38 +0300

    Add framebuffer attachments + spec up

--- lib/dengine-core/include/dengine/framebuffer.h
+++ lib/dengine-core/include/dengine/framebuffer.h
@@ -3,9 +3,25 @@
 
 #include <stddef.h> //size_t
 #include <stdint.h> //uint32
+
+#include "dengine/texture.h"
+#include "dengine/loadgl.h"
+typedef enum FramebufferAttachmentType
+{
+    DENGINE_FRAMEBUFFER_COLOR = GL_COLOR_ATTACHMENT0,
+    DENGINE_FRAMEBUFFER_DEPTH = GL_DEPTH_ATTACHMENT,
+    DENGINE_FRAMEBUFFER_STENCIL = GL_STENCIL_ATTACHMENT
+}FramebufferAttachmentType;
+
 typedef struct Framebuffer
 {
     uint32_t framebuffer_id;
+
+    Texture* color;
+    uint32_t n_color;
+
+    Texture depth;
+    Texture stencil;
 }Framebuffer;
 
 #ifdef __cplusplus
@@ -16,6 +32,9 @@ void dengine_framebuffer_gen(size_t count, Framebuffer* framebuffers);
 
 void dengine_framebuffer_bind(uint32_t target, Framebuffer* framebuffer);
 
+void dengine_framebuffer_attach(FramebufferAttachmentType attachment, Texture* texture, Framebuffer* framebuffer);
+
+void dengine_framebuffer_attach2D(FramebufferAttachmentType attachment, Texture* texture, Framebuffer* framebuffer);
 #ifdef __cplusplus
 }
 #endif
--- lib/dengine-core/src/framebuffer.c
+++ lib/dengine-core/src/framebuffer.c
@@ -2,13 +2,54 @@
 
 #include "loadgl.h"
 
+#include <string.h> //memset
 void dengine_framebuffer_gen(size_t count, Framebuffer* framebuffers)
 {
     for(size_t i = 0; i < count; i++)
+    {
+        memset(&framebuffers[i], 0, sizeof(Framebuffer));
         glGenFramebuffers(1, &framebuffers[i].framebuffer_id); DENGINE_CHECKGL;
+    }
 }
 
 void dengine_framebuffer_bind(uint32_t target, Framebuffer* framebuffer)
 {
-    glBindFramebuffer(target, framebuffer->framebuffer_id); DENGINE_CHECKGL;
+    if(framebuffer)
+        glBindFramebuffer(target, framebuffer->framebuffer_id);
+    else
+        glBindFramebuffer(target, 0);
+
+    DENGINE_CHECKGL;
+}
+
+void dengine_framebuffer_attach(FramebufferAttachmentType attachment, Texture* texture, Framebuffer* framebuffer)
+{
+#if defined(DENGINE_GL_GLAD) || defined(DENGINE_GL_GLES32)
+    if(attachment == DENGINE_FRAMEBUFFER_COLOR){
+        //on GL/ES 3.2+
+        glFramebufferTexture(GL_FRAMEBUFFER, attachment + framebuffer->n_color, texture->texture_id, 0);
+        if(!DENGINE_CHECKGL)
+            framebuffer->n_color++;
+    }
+    else{
+        //on GL/ES 3.2+
+        glFramebufferTexture(GL_FRAMEBUFFER, attachment, texture->texture_id, 0);
+        DENGINE_CHECKGL;
+    }
+#endif
+}
+
+void dengine_framebuffer_attach2D(FramebufferAttachmentType attachment, Texture* texture, Framebuffer* framebuffer)
+{
+    if(attachment == DENGINE_FRAMEBUFFER_COLOR){
+        //on GL 3.0 / ES 2.0+
+        glFramebufferTexture2D(GL_FRAMEBUFFER, attachment + framebuffer->n_color, GL_TEXTURE_2D, texture->texture_id, 0);
+        if(!DENGINE_CHECKGL)
+            framebuffer->n_color++;
+    }
+    else{
+        //on GL 3.0 / ES 2.0+
+        glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, GL_TEXTURE_2D, texture->texture_id, 0);
+        DENGINE_CHECKGL;
+    }
 }
