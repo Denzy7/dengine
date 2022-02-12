55e1b478bc3a96957a7dd18ad4d63536c764414d
Denzy7 <dennismyner7@gmail.com>
2022-02-12 18:00:40 +0300

    Migrate gui android test to use core functions

--- tests/src/testdengine-android/testdengineguigui/app/src/main/code/main.c
+++ tests/src/testdengine-android/testdengineguigui/app/src/main/code/main.c
@@ -1,46 +1,17 @@
-#include <android_native_app_glue.h>
-#include <jni.h>
-#include <android/asset_manager.h>
-
+#include <dengine/android.h>
 #include <dengine/window.h>
-#include <dengine-utils/logging.h>
 #include <dengine/loadgl.h>
-#include <dengine-utils/timer.h>
 
-#include <dengine/primitive.h>
-#include <dengine/draw.h>
-#include <dengine/shader.h>
-#include <string.h>
-#include <dengine/camera.h>
-#include <cglm/cglm.h>
-double elapsed;
+#include <dengine-utils/timer.h>
+#include <dengine-utils/logging.h>
 
 #include <dengine-gui/gui.h>
 
-AAssetManager* asset_mgr;
-
-void* a2m(const char* file, size_t* sz)
-{
-    if(!asset_mgr)
-        return NULL;
-    AAsset* asset = AAssetManager_open(asset_mgr, file, AASSET_MODE_BUFFER);
-    if(!asset)
-        return NULL;
-
-    *sz = AAsset_getLength(asset);
-
-    //nullterm for txt
-    void* mem = malloc(*sz + 1);
-    memset(mem, 0, *sz + 1);
-    AAsset_read(asset, mem, *sz);
-    AAsset_close(asset);
-    return mem;
-}
-
+double elapsed;
+int window_init = 0;
 
 static void init(struct android_app* app)
 {
-    //Acquire win
     ANativeWindow_acquire(app->window);
     dengine_window_android_set_nativewindow(app->window);
 
@@ -48,6 +19,8 @@ static void init(struct android_app* app)
 
     if(dengine_window_init())
     {
+        window_init = 1;
+
         dengineutils_logging_log("init window success");
 
         dengineutils_logging_log("GL : %s", glGetString(GL_VERSION));
@@ -55,18 +28,21 @@ static void init(struct android_app* app)
         if(denginegui_init())
             dengineutils_logging_log("init gui");
 
-        size_t sz = 0;
-        void* ttf = a2m("OpenSans-Regular.ttf", &sz);
-        if(denginegui_set_font(ttf, 50.0f, 512))
-            free(ttf);
+        File2Mem f2m;
+        f2m.file = "OpenSans-Regular.ttf";
+        int rd =dengine_android_asset2file2mem(&f2m);
+        dengineutils_logging_log("rd %d", rd);
+
+        if(denginegui_set_font(f2m.mem, 50.0f, 512))
+            dengineutils_filesys_file2mem_free(&f2m);
     }
 }
 
-static void destroy(struct android_app* app)
+static void term(struct  android_app* app)
 {
     dengine_window_terminate();
-
     ANativeWindow_release(app->window);
+    ANativeActivity_finish(app->activity);
 }
 
 static void draw()
@@ -78,85 +54,24 @@ static void draw()
 
     float rgba[] = {1.0f, 1.0f, 0.0f, 1.0f};
     denginegui_text(50.0f, 50.0f, "Colored Text...~", rgba);
+    denginegui_button(100.0f, 100.0f, 300.0f, 200.0f, "Click Me!", NULL);
 
     dengine_window_swapbuffers();
 }
 
-static void cmd_handle(struct android_app* app, int32_t cmd)
+void android_main(struct android_app* app)
 {
-    switch (cmd) {
-        case APP_CMD_SAVE_STATE:
-            dengineutils_logging_log("Saving state...");
-            break;
-
-        case APP_CMD_INIT_WINDOW:
-            dengineutils_logging_log("Getting window ready...");
-            init(app);
-            //Buggy fullscreen
-            //ANativeActivity_setWindowFlags(app->activity, AWINDOW_FLAG_FULLSCREEN, 0);
-            break;
-
-        case APP_CMD_TERM_WINDOW:
-            dengineutils_logging_log("Term window");
-            destroy(app);
-            break;
-
-        case APP_CMD_GAINED_FOCUS:
-            dengineutils_logging_log("Gained focus");
-            break;
-
-        case APP_CMD_LOST_FOCUS:
-            dengineutils_logging_log("Lost focus");
-            break;
-
-        case APP_CMD_PAUSE:
-            dengineutils_logging_log("Paused");
-            break;
-
-        case APP_CMD_RESUME:
-            dengineutils_logging_log("Resumed");
-            break;
-
-        case APP_CMD_DESTROY:
-            dengineutils_logging_log("Destroy");
-            break;
-    }
-}
-
-void android_main(struct android_app* state)
-{
-    //Set app callbacks
-    state->onAppCmd = cmd_handle;
-    asset_mgr = state->activity->assetManager;
-
-    if(state->savedState)
-    {
-        dengineutils_logging_log("Restoring from save state");
-    }
+    dengine_android_set_app(app);
+    dengine_android_set_initfunc(init);
+    dengine_android_set_terminatefunc(term);
 
     while(1)
     {
-        //Read events
-        int events;
-        struct android_poll_source* source;
-
-        while((ALooper_pollAll(1 ? 0 : -1, NULL, &events, (void**)&source)) >= 0)
-        {
-            //Process event
-            if(source != NULL)
-            {
-                source->process(state, source);
-            }
-        }
+        dengine_android_pollevents();
 
         //Quit and detach
-        if(state->destroyRequested != 0)
+        if(app->destroyRequested != 0)
         {
-            dengineutils_logging_log("Destroy Requested");
-            ANativeActivity_finish(state->activity);
-
-            //state->activity->vm->DetachCurrentThread();
-
             dengineutils_logging_log("Goodbye!");
             return;
         }
@@ -170,7 +85,7 @@ void android_main(struct android_app* state)
             dengineutils_logging_log("step");
             elapsed = 0;
         }
-
-        draw();
+        if(window_init)
+            draw();
     }
 }
