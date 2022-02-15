dfaf5a9c4449577f46f309ec6e89f1a7d739b507
Denzy7 <dennismyner7@gmail.com>
2022-02-15 16:08:35 +0300

    VAO implementation

---
+++ lib/dengine-core/src/vao.c
@@ -0,0 +1,37 @@
+#include "dengine/vao.h"
+
+#include <string.h> //memset
+
+#include "dengine/loadgl.h"
+void dengine_vao_gen(size_t count, VAO* vaos)
+{
+    for(size_t i = 0; i < count; i++)
+    {
+        //Check for possible PFNs
+        if(glad_glGenVertexArrays)
+            glGenVertexArrays(1, &vaos[i].vao);
+        else if(glad_glGenVertexArraysOES)
+            glGenVertexArraysOES(1, &vaos[i].vao);
+        else if(glad_glGenVertexArraysAPPLE)
+            glGenVertexArraysAPPLE(1, &vaos[i].vao);
+
+        DENGINE_CHECKGL;
+    }
+}
+
+void dengine_vao_bind(VAO* vao)
+{
+    uint32_t _vao = 0;
+    if(vao)
+        _vao = vao->vao;
+
+    //Check for possible PFNs
+    if(glad_glBindVertexArray)
+        glBindVertexArray(_vao);
+    else if(glad_glBindVertexArrayOES)
+        glBindVertexArrayOES(_vao);
+    else if(glad_glBindVertexArrayAPPLE)
+        glBindVertexArrayAPPLE(_vao);
+
+    DENGINE_CHECKGL;
+}
