720957d72298ce7a6d688a4b21a5798ed493b615
Denzy7 <dennismyner7@gmail.com>
2022-03-17 07:50:17 +0300

    extern model.h

--- lib/dengine-model/include/dengine-model/model.h
+++ lib/dengine-model/include/dengine-model/model.h
@@ -1,3 +1,5 @@
+#ifndef MODEL_H
+#define MODEL_H
 #include "dengine/primitive.h"
 
 #include <stdint.h> //uint16,32
@@ -7,4 +9,13 @@ typedef enum DengineModelFormat
      DENGINE_MODEL_FORMAT_OBJ
 }DengineModelFormat;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int denginemodel_load_file(DengineModelFormat format, const char* file, Primitive* primitive, Shader* shader);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
