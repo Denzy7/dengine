e394b5d621dd1d180e41396b108ead2fcabc095e
Denzy7 <dennismyner7@gmail.com>
2022-03-23 16:16:58 +0300

    add renderbuffer impl.

---
+++ lib/dengine-core/include/dengine/renderbuffer.h
@@ -0,0 +1,23 @@
+#ifndef RENDERBUFFER_H
+#define RENDERBUFFER_H
+
+#include <stdint.h> //uint32_t
+#include <stddef.h> //size_t
+typedef struct
+{
+    uint32_t renderbuffer_id;
+
+    uint32_t format;
+    uint32_t width;
+    uint32_t height;
+}Renderbuffer;
+
+void dengine_renderbuffer_gen(const size_t count, Renderbuffer* renderbuffers);
+
+void dengine_renderbuffer_destroy(const size_t count, Renderbuffer* renderbuffers);
+
+void dengine_renderbuffer_bind(const Renderbuffer* renderbuffer);
+
+void dengine_renderbuffer_data(const Renderbuffer* renderbuffer);
+
+#endif // RENDERBUFFER_H
---
+++ lib/dengine-core/src/renderbuffer.c
@@ -0,0 +1,53 @@
+#include "dengine/renderbuffer.h"
+#include "dengine/loadgl.h" //GL
+
+void dengine_renderbuffer_gen(const size_t count, Renderbuffer* renderbuffers)
+{
+    for(size_t i = 0; i < count; i++)
+    {
+        if(glad_glGenRenderbuffers)
+            glGenRenderbuffers(1, &renderbuffers->renderbuffer_id);
+        else if(glad_glGenRenderbuffersEXT)
+            glGenRenderbuffersEXT(1, &renderbuffers->renderbuffer_id);
+
+        DENGINE_CHECKGL;
+    }
+}
+
+void dengine_renderbuffer_destroy(const size_t count, Renderbuffer* renderbuffers)
+{
+    for(size_t i = 0; i < count; i++)
+    {
+        if(glad_glDeleteRenderbuffers)
+            glDeleteRenderbuffers(1, &renderbuffers->renderbuffer_id);
+        else if(glad_glDeleteRenderbuffersEXT)
+            glDeleteRenderbuffersEXT(1, &renderbuffers->renderbuffer_id);
+
+        DENGINE_CHECKGL;
+    }
+}
+
+void dengine_renderbuffer_bind(const Renderbuffer* renderbuffer)
+{
+    if(glad_glBindRenderbuffer)
+        glBindRenderbuffer(GL_RENDERBUFFER, renderbuffer->renderbuffer_id);
+    else if(glad_glBindRenderbufferEXT)
+        glBindRenderbufferEXT(GL_RENDERBUFFER, renderbuffer->renderbuffer_id);
+
+    DENGINE_CHECKGL;
+}
+
+void dengine_renderbuffer_data(const Renderbuffer* renderbuffer)
+{
+    if(glad_glRenderbufferStorage)
+    {
+        glRenderbufferStorage(GL_RENDERBUFFER, renderbuffer->format,
+                              renderbuffer->width, renderbuffer->height);
+    }else if(glad_glRenderbufferStorageEXT)
+    {
+        glRenderbufferStorageEXT(GL_RENDERBUFFER, renderbuffer->format,
+                              renderbuffer->width, renderbuffer->height);
+    }
+
+    DENGINE_CHECKGL;
+}
