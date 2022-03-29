8663bd66b1f37eb7c5f662ae40afce01e0ae4395
Denzy7 <dennismyner7@gmail.com>
2022-03-29 06:02:47 +0300

    log with utils

--- assets/scripts/printmouse.py
+++ assets/scripts/printmouse.py
@@ -1,17 +1,27 @@
 import dengine.inpt as inpt
 import dengine.timer as timer
+import dengine.logging as logging
+
 elapsed = 0.0
+
 def start():
-    print("Start called")
+    logging.log("INFO::start called")
 
 def update():
     global elapsed
     elapsed += timer.get_delta()
     if(elapsed > 1000.0):
-        print("mouse x", inpt.get_mousex())
-        print("mouse y", inpt.get_mousey())
+        x = "mouse x " + str(inpt.get_mousex())
+        logging.log(x)
+
+        y = "mouse y " + str(inpt.get_mousey())
+        logging.log(y)
+
         elapsed = 0.0
 
     if(inpt.get_key('A')):
-        print("pressed a")
+        logging.log("WARNING::pressed a")
+
+    if(inpt.get_key('X')):
+        logging.log("ERROR::pressed x")
 
