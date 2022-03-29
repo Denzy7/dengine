58518f7fda701ac3355fa7f2da18f47b785c112c
Denzy7 <dennismyner7@gmail.com>
2022-03-29 09:29:48 +0300

    write a file using filesys module

--- assets/scripts/printmouse.py
+++ assets/scripts/printmouse.py
@@ -1,12 +1,19 @@
 import dengine.inpt as inpt
 import dengine.timer as timer
 import dengine.logging as logging
-
+import dengine.filesys as filesys
+import time
 elapsed = 0.0
 
 
 def start():
     logging.log("INFO::start called")
+    cd = filesys.get_cachedir()
+    fd = filesys.get_filesdir()
+    print("cache:", cd, "files", fd)
+    with open(cd+"/try.file", "w") as f:
+        t = time.strftime("%d-%m-%Y, %H:%M:%S")
+        f.write("hello!"+t)
 
 
 def update():
