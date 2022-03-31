44c2dd2ce2d306e6cdeb60cd052f663dfc4a87e7
Denzy7 <dennismyner7@gmail.com>
2022-03-31 22:54:16 +0300

    extern "C" for C++

--- lib/dengine-utils/include/dengine-utils/debug.h
+++ lib/dengine-utils/include/dengine-utils/debug.h
@@ -3,6 +3,10 @@
 
 #define DENGINE_DEBUG_ENTER dengineutils_debug_enter(__FUNCTION__,__FILE__,__LINE__)
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void dengineutils_debug_init();
 
 void dengineutils_debug_terminate();
@@ -12,5 +16,9 @@ void dengineutils_debug_enter(const char* function,const char* file,const int li
 void dengineutils_debug_trace_push(const char* str);
 
 void dengineutils_debug_trace_dump();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // DEBUG_H
