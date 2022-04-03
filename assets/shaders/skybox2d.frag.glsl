14e8a777aba08981acba5b873968be215cbee3e4
Denzy7 <dennismyner7@gmail.com>
2022-04-03 14:29:43 +0300

    pull back some identation

--- assets/shaders/skybox2d.frag.glsl
+++ assets/shaders/skybox2d.frag.glsl
@@ -8,15 +8,15 @@ vec2 invAtan = vec2(0.1591, 0.3183);
 
 vec2 SampleSphericalMap(vec3 v)
 {
-	vec2 uv = vec2(atan(v.z, v.x), asin(v.y));
-	uv *= invAtan;
-	uv += 0.5;
-	return uv;
+    vec2 uv = vec2(atan(v.z, v.x), asin(v.y));
+    uv *= invAtan;
+    uv += 0.5;
+    return uv;
 }
 
 void main()
 {
-        vec2 uv = SampleSphericalMap(normalize(TexCoord)); // make sure to normalize
-        vec3 col = texture2D(eqireqMap,uv).rgb;
-        gl_FragColor = vec4(col, 1.0);
+    vec2 uv = SampleSphericalMap(normalize(TexCoord)); // make sure to normalize
+    vec3 col = texture2D(eqireqMap,uv).rgb;
+    gl_FragColor = vec4(col, 1.0);
 }
