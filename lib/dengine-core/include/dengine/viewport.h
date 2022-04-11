1f83aa85a5ae8af7a197bba73b6dc47a1b5d3dfd
Denzy7 <dennismyner7@gmail.com>
2022-04-11 12:48:12 +0300

    add get set viewport

---
+++ lib/dengine-core/include/dengine/viewport.h
@@ -0,0 +1,18 @@
+#ifndef VIEWPORT_H
+#define VIEWPORT_H
+
+#include "dengine/loadgl.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void dengine_viewport_get(int* x, int* y, int* width, int* height);
+
+void dengine_viewport_set(int x, int y, int width, int height);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // VIEWPORT_H
---
+++ lib/dengine-core/src/viewport.c
@@ -0,0 +1,22 @@
+#include "dengine/viewport.h"
+
+void dengine_viewport_get(int* x, int* y, int* width, int* height)
+{
+    int viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+
+    if(x)
+        *x = viewport[0];
+    if(y)
+        *y = viewport[1];
+    if(width)
+        *width = viewport[2];
+    if(height)
+        *height = viewport[3];
+
+}
+
+void dengine_viewport_set(int x, int y, int width, int height)
+{
+    glViewport(x, y, width, height);
+}
