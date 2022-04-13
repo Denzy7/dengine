1ca85fa5a6cf01a085d23d880a9cbd7f4ac66fe9
Denzy7 <dennismyner7@gmail.com>
2022-04-13 19:27:37 +0300

    add embfont roboto

---
+++ lib/dengine-gui/include/dengine-gui/embfonts/roboto/roboto_light.h
@@ -0,0 +1,7 @@
+#ifndef ROBOTO_LIGHT_H
+#define ROBOTO_LIGHT_H
+
+extern const unsigned int Roboto_Light_ttf_len;
+extern unsigned char Roboto_Light_ttf[];
+
+#endif // ROBOTO_LIGHT_H
---
+++ lib/dengine-gui/include/dengine-gui/embfonts/roboto/roboto_regular.h
@@ -0,0 +1,7 @@
+#ifndef ROBOTO_REGULAR_H
+#define ROBOTO_REGULAR_H
+
+extern const unsigned int Roboto_Regular_ttf_len;
+extern unsigned char Roboto_Regular_ttf[];
+
+#endif // ROBOTO_REGULAR_H
