669ce85d7a0a1f41e682cb8b46fd92e06eff4724
Denzy7 <dennismyner7@gmail.com>
2022-04-17 12:33:49 +0300

    pull ecs data to inspector widgets

--- main/dengitor/src/inspector.c
+++ main/dengitor/src/inspector.c
@@ -39,8 +39,72 @@ void dengitor_inspector_do_entity(Entity* entity, Inspector* inspector)
         return;
 
     gtk_widget_show_all(inspector->transform_widget.transform);
+
+    //pull transform data
+    GList* pos_list = gtk_container_get_children(inspector->transform_widget.transform_position);
+    GList* rot_list = gtk_container_get_children(inspector->transform_widget.transform_rotation);
+    GList* scl_list = gtk_container_get_children(inspector->transform_widget.transform_scale);
+
+    GtkEntry* entry;
+    //const char* text;
+    GtkEntryBuffer* buffer;
+    char prtbf[1024];
+
+    for(guint i = 0; i < 3; i++)
+    {
+        GList* pos_nth = g_list_nth(pos_list, i);
+        GList* rot_nth = g_list_nth(rot_list, i);
+        GList* scl_nth = g_list_nth(scl_list, i);
+
+        // pos
+        entry = GTK_ENTRY(pos_nth->data);
+        buffer = gtk_entry_get_buffer(entry);
+        g_snprintf(prtbf, sizeof(prtbf), "%.1f", entity->transform.position[i]);
+        gtk_entry_buffer_set_text(buffer, prtbf, strlen(prtbf));
+
+        //rot
+        entry = GTK_ENTRY(rot_nth->data);
+        buffer = gtk_entry_get_buffer(entry);
+        g_snprintf(prtbf, sizeof(prtbf), "%.1f", entity->transform.rotation[i]);
+        gtk_entry_buffer_set_text(buffer, prtbf, strlen(prtbf));
+
+        //scl
+        entry = GTK_ENTRY(scl_nth->data);
+        buffer = gtk_entry_get_buffer(entry);
+        g_snprintf(prtbf, sizeof(prtbf), "%.1f", entity->transform.scale[i]);
+        gtk_entry_buffer_set_text(buffer, prtbf, strlen(prtbf));
+    }
+
     if(entity->camera_component)
     {
+        Camera* camera = entity->camera_component->camera;
         gtk_widget_show_all(inspector->camera_widget.camera);
+
+        // fov
+        gtk_adjustment_set_value(
+                    inspector->camera_widget.camera_fov,
+                    camera->fov);
+
+        //near
+        entry = inspector->camera_widget.camera_near;
+        buffer = gtk_entry_get_buffer(entry);
+        g_snprintf(prtbf, sizeof(prtbf), "%.1f", camera->near);
+        gtk_entry_buffer_set_text(buffer, prtbf, strlen(prtbf));
+
+        //far
+        entry = inspector->camera_widget.camera_far;
+        buffer = gtk_entry_get_buffer(entry);
+        g_snprintf(prtbf, sizeof(prtbf), "%.1f", camera->far);
+        gtk_entry_buffer_set_text(buffer, prtbf, strlen(prtbf));
+
+        //clear
+        GdkRGBA rgba = {camera->clearcolor[0],
+                        camera->clearcolor[1],
+                        camera->clearcolor[2],
+                        camera->clearcolor[3],
+                       };
+        gtk_color_chooser_set_rgba(
+                    GTK_COLOR_CHOOSER(inspector->camera_widget.camera_clearcolour),
+                    &rgba);
     }
 }
