cc9636481ed26c85a375d1b0543f9163f67af32d
Denzy7 <dennismyner7@gmail.com>
2022-04-24 10:03:05 +0300

    w2v for GtkAdjustment

--- main/dengitor/include/dengitor/w2v.h
+++ main/dengitor/include/dengitor/w2v.h
@@ -17,4 +17,6 @@ void dengitor_w2v_set_glarea(GtkGLArea* area);
 
 void dengitor_w2v_entry2float(GtkEntry* entry, float* var);
 
+void dengitor_w2v_adjustment2float(GtkAdjustment* adjustment, float* var);
+
 #endif // W2V_H
--- main/dengitor/src/w2v.c
+++ main/dengitor/src/w2v.c
@@ -19,3 +19,11 @@ void dengitor_w2v_entry2float(GtkEntry* entry, float* var)
         gtk_widget_queue_draw( GTK_WIDGET(_area) );
 
 }
+
+void dengitor_w2v_adjustment2float(GtkAdjustment* adjustment, float* var)
+{
+    *var = gtk_adjustment_get_value(adjustment);
+
+    if(_area)
+        gtk_widget_queue_draw( GTK_WIDGET(_area) );
+}
