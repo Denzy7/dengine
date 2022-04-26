a437e38edf7c299b9e2562bd942eae739ad7c02a
Denzy7 <dennismyner7@gmail.com>
2022-04-26 17:32:26 +0300

    signal utils for finding and disconnect

--- main/dengitor/include/dengitor/utils.h
+++ main/dengitor/include/dengitor/utils.h
@@ -5,4 +5,8 @@
 
 void dengitor_utils_changecontainer(GtkWidget* widget, GtkContainer* container_old, GtkContainer* container_new);
 
+gulong dengitor_utils_get_signal(GType type, gpointer instance, const char* name);
+
+void dengitor_utils_disconnect(GType type, gpointer instance, const char* signal);
+
 #endif // UTILS_H
--- main/dengitor/src/utils.c
+++ main/dengitor/src/utils.c
@@ -7,3 +7,18 @@ void dengitor_utils_changecontainer(GtkWidget* widget, GtkContainer* container_o
     gtk_container_add(container_new, widget);
     g_object_unref(widget);
 }
+
+gulong dengitor_utils_get_signal(GType type, gpointer instance, const char* name)
+{
+    guint id;
+    GQuark detail;
+    g_signal_parse_name(name, type, &id, &detail, FALSE);
+    return g_signal_handler_find(instance, G_SIGNAL_MATCH_ID, id, detail, NULL, NULL, NULL);
+}
+
+void dengitor_utils_disconnect(GType type, gpointer instance, const char* signal)
+{
+    gulong sig = dengitor_utils_get_signal(type, instance, signal);
+    if(sig)
+        g_signal_handler_disconnect(instance, sig);
+}
