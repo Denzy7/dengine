9a127d1b0943f9c910a8d4764cc7cbcd93b74c30
Denzy7 <dennismyner7@gmail.com>
2022-04-27 11:06:30 +0300

    w2v adjust2int, combo2int

--- main/dengitor/include/dengitor/w2v.h
+++ main/dengitor/include/dengitor/w2v.h
@@ -13,6 +13,14 @@ void dengitor_w2v_entry2float(GtkEntry* entry, float* var);
 
 void dengitor_w2v_adjustment2float(GtkAdjustment* adjustment, float* var);
 
+void dengitor_w2v_adjustment2int(GtkAdjustment* adjustment, int* var);
+
 void dengitor_w2v_colorbtn2float4(GtkColorButton* colbtn, float* var);
+
+void dengitor_w2v_combo2int(GtkComboBox* combo, int* var);
+
+void dengitor_w2v_toggle2int(GtkToggleButton* toggle, int* var);
+
+void dengitor_w2v_check2int(GtkCheckButton* check, int* var);
 
 #endif // W2V_H
--- main/dengitor/src/w2v.c
+++ main/dengitor/src/w2v.c
@@ -14,6 +14,12 @@ void dengitor_w2v_adjustment2float(GtkAdjustment* adjustment, float* var)
     *var = gtk_adjustment_get_value(adjustment);
 }
 
+void dengitor_w2v_adjustment2int(GtkAdjustment* adjustment, int* var)
+{
+    float v = gtk_adjustment_get_value(adjustment);
+    *var = (int)v;
+}
+
 void dengitor_w2v_colorbtn2float4(GtkColorButton* colbtn, float* var)
 {
     GdkRGBA col;
@@ -23,4 +29,19 @@ void dengitor_w2v_colorbtn2float4(GtkColorButton* colbtn, float* var)
     var[1] = col.green;
     var[2] = col.blue;
     var[3] = col.alpha;
+}
+
+void dengitor_w2v_combo2int(GtkComboBox* combo, int* var)
+{
+    *var = gtk_combo_box_get_active(combo);
+}
+
+void dengitor_w2v_toggle2int(GtkToggleButton* toggle, int* var)
+{
+    *var = gtk_toggle_button_get_active(toggle);
+}
+
+void dengitor_w2v_check2int(GtkCheckButton* check, int* var)
+{
+    dengitor_w2v_toggle2int( GTK_TOGGLE_BUTTON(check), var);
 }
