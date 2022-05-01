1a74c4523eed429e32f45308b6632e53dd932aef
Denzy7 <dennismyner7@gmail.com>
2022-05-01 21:42:15 +0300

    export commonmodule.h objects like Vec3

--- lib/dengine-script/src/py_modules/commonmodule.c
+++ lib/dengine-script/src/py_modules/commonmodule.c
@@ -1,4 +1,37 @@
 #include "commonmodule.h"
+#include <structmember.h>
+
+static PyMemberDef Vec3Object_Members[]=
+{
+    {"x", T_DOUBLE, offsetof(Vec3Object, x), 0, "X component"},
+    {"y", T_DOUBLE, offsetof(Vec3Object, y), 0, "Y component"},
+    {"z", T_DOUBLE, offsetof(Vec3Object, z), 0, "Z component"},
+    {NULL}
+};
+
+PyTypeObject Vec3Object_Type=
+{
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "common.Vec3",
+    .tp_doc = "Vector with 3 components (X, Y and Z)",
+    .tp_basicsize = sizeof(Vec3Object),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    .tp_members = Vec3Object_Members,
+
+    .tp_new = PyType_GenericNew,
+    .tp_alloc = PyType_GenericAlloc,
+    .tp_getattro = PyObject_GenericGetAttr,
+    .tp_setattro = PyObject_GenericSetAttr
+};
+
+static PyModuleDef commonmodule=
+{
+    PyModuleDef_HEAD_INIT,
+    "common",
+    "Dengine common scripting types and methods",
+    -1
+};
 
 PyMODINIT_FUNC PyInit_common()
 {
--- lib/dengine-script/src/py_modules/commonmodule.h
+++ lib/dengine-script/src/py_modules/commonmodule.h
@@ -1,17 +1,11 @@
 #ifndef COMMONMODULE_H
 #define COMMONMODULE_H
 
-#include "dengine-script/script.h"
-#include <structmember.h>
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
 
 #ifdef __cplusplus
 extern "C" {
-#endif
-
-PyMODINIT_FUNC PyInit_common();
-
-#ifdef __cplusplus
-}
 #endif
 
 typedef struct
@@ -22,36 +16,10 @@ typedef struct
     double z;
 }Vec3Object;
 
-static PyMemberDef Vec3Object_Members[]=
-{
-    {"x", T_DOUBLE, offsetof(Vec3Object, x), 0, "X component"},
-    {"y", T_DOUBLE, offsetof(Vec3Object, y), 0, "Y component"},
-    {"z", T_DOUBLE, offsetof(Vec3Object, z), 0, "Z component"},
-    {NULL}
-};
+PyAPI_DATA(PyTypeObject) Vec3Object_Type;
 
-static PyTypeObject Vec3Object_Type=
-{
-    PyVarObject_HEAD_INIT(NULL, 0)
-    .tp_name = "common.Vec3",
-    .tp_doc = "Vector with 3 components (X, Y and Z)",
-    .tp_basicsize = sizeof(Vec3Object),
-    .tp_itemsize = 0,
-    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
-    .tp_members = Vec3Object_Members,
-
-    .tp_new = PyType_GenericNew,
-    .tp_alloc = PyType_GenericAlloc,
-    .tp_getattro = PyObject_GenericGetAttr,
-    .tp_setattro = PyObject_GenericSetAttr
-};
-
-static PyModuleDef commonmodule=
-{
-    PyModuleDef_HEAD_INIT,
-    "common",
-    "Dengine common scripting types and methods",
-    -1
-};
+#ifdef __cplusplus
+}
+#endif
 
 #endif // COMMONMODULE_H
