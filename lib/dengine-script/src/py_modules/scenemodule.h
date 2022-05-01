73fd0f77e8c95ea759f24c8144fb4dba27a1e510
Denzy7 <dennismyner7@gmail.com>
2022-05-01 21:40:28 +0300

    remove unneded scenemodule [will be back later]

--- lib/dengine-script/src/py_modules/scenemodule.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCENEMODULE_H
-#define SCENEMODULE_H
-
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
-
-PyObject* EntityObject_new(PyTypeObject* type, PyObject* args, PyObject* kwds);
-
-#endif // SCENEMODULE_H
