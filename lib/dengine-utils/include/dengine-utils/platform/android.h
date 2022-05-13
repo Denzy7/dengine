bff928a252d8ee4756bfd702c9745a2196d0b317
Denzy7 <dennismyner7@gmail.com>
2022-05-13 08:47:21 +0300

    add android platform utils

---
+++ lib/dengine-utils/include/dengine-utils/platform/android.h
@@ -0,0 +1,34 @@
+#ifndef ANDROID_H
+#define ANDROID_H
+
+#include <android/native_app_glue/android_native_app_glue.h>
+
+#include "dengine-utils/filesys.h" //f2m
+
+typedef void (*DengineAndroidAppFunc)(struct android_app*);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void dengineutils_android_set_app(struct android_app* app);
+
+void dengineutils_android_set_initfunc(DengineAndroidAppFunc func);
+
+void dengineutils_android_set_terminatefunc(DengineAndroidAppFunc func);
+
+void dengineutils_android_pollevents();
+
+void dengineutils_android_set_filesdir();
+
+void dengineutils_android_set_cachedir();
+
+int dengineutils_android_asset2file2mem(File2Mem* f2m);
+
+ANativeWindow* dengineutils_android_get_window();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ANDROID_H
