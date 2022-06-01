08c5a83dceb869babfc58550b995f0631e8e471d
Denzy7 <dennismyner7@gmail.com>
2022-06-01 22:28:21 +0300

    block vao's for gl & es 3.2

--- lib/dengine-core/src/vao.c
+++ lib/dengine-core/src/vao.c
@@ -5,49 +5,58 @@
 #include "dengine/loadgl.h"
 void dengine_vao_gen(size_t count, VAO* vaos)
 {
-    for(size_t i = 0; i < count; i++)
+    if(GLAD_GL_VERSION_3_2 || GLAD_GL_ES_VERSION_3_2)
     {
-        //Check for possible PFNs
-        if(glad_glGenVertexArrays)
-            glGenVertexArrays(1, &vaos[i].vao);
-        else if(glad_glGenVertexArraysOES)
-            glGenVertexArraysOES(1, &vaos[i].vao);
-        else if(glad_glGenVertexArraysAPPLE)
-            glGenVertexArraysAPPLE(1, &vaos[i].vao);
+        for(size_t i = 0; i < count; i++)
+        {
+            //Check for possible PFNs
+            if(glad_glGenVertexArrays)
+                glGenVertexArrays(1, &vaos[i].vao);
+            else if(glad_glGenVertexArraysOES)
+                glGenVertexArraysOES(1, &vaos[i].vao);
+            else if(glad_glGenVertexArraysAPPLE)
+                glGenVertexArraysAPPLE(1, &vaos[i].vao);
 
-        DENGINE_CHECKGL;
+            DENGINE_CHECKGL;
+        }
     }
 }
 
 void dengine_vao_bind(const VAO* vao)
 {
-    uint32_t _vao = 0;
-    if(vao)
-        _vao = vao->vao;
+    if(GLAD_GL_VERSION_3_2 || GLAD_GL_ES_VERSION_3_2)
+    {
+        uint32_t _vao = 0;
+        if(vao)
+            _vao = vao->vao;
 
-    //Check for possible PFNs
-    if(glad_glBindVertexArray)
-        glBindVertexArray(_vao);
-    else if(glad_glBindVertexArrayOES)
-        glBindVertexArrayOES(_vao);
-    else if(glad_glBindVertexArrayAPPLE)
-        glBindVertexArrayAPPLE(_vao);
+        //Check for possible PFNs
+        if(glad_glBindVertexArray)
+            glBindVertexArray(_vao);
+        else if(glad_glBindVertexArrayOES)
+            glBindVertexArrayOES(_vao);
+        else if(glad_glBindVertexArrayAPPLE)
+            glBindVertexArrayAPPLE(_vao);
 
-    DENGINE_CHECKGL;
+        DENGINE_CHECKGL;
+    }
 }
 
 void dengine_vao_destroy(size_t count, VAO* vaos)
 {
-    for(size_t i = 0; i < count; i++)
+    if(GLAD_GL_VERSION_3_2 || GLAD_GL_ES_VERSION_3_2)
     {
-        //Check for possible PFNs
-        if(glad_glDeleteVertexArrays)
-            glDeleteVertexArrays(1, &vaos[i].vao);
-        else if(glad_glDeleteVertexArrays)
-            glDeleteVertexArraysOES(1, &vaos[i].vao);
-        else if(glad_glDeleteVertexArraysAPPLE)
-            glDeleteVertexArraysAPPLE(1, &vaos[i].vao);
+        for(size_t i = 0; i < count; i++)
+        {
+            //Check for possible PFNs
+            if(glad_glDeleteVertexArrays)
+                glDeleteVertexArrays(1, &vaos[i].vao);
+            else if(glad_glDeleteVertexArrays)
+                glDeleteVertexArraysOES(1, &vaos[i].vao);
+            else if(glad_glDeleteVertexArraysAPPLE)
+                glDeleteVertexArraysAPPLE(1, &vaos[i].vao);
 
-        DENGINE_CHECKGL;
+            DENGINE_CHECKGL;
+        }
     }
 }
