6e86379bddb91e5595a3d745690205729db361b1
Denzy7 <dennismyner7@gmail.com>
2022-06-03 11:16:25 +0300

    extern "C" for CXX

--- lib/dengine-utils/include/dengine-utils/dynlib.h
+++ lib/dengine-utils/include/dengine-utils/dynlib.h
@@ -4,9 +4,18 @@
 #include "dengine_config.h"
 typedef void* DynLib;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 DynLib dengineutils_dynlib_open(const char* path);
 
 void dengineutils_dynlib_close(DynLib dynlib);
 
 void* dengineutils_dynlib_get_sym(const DynLib dynlib, const char* name);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif // DYNLIB_H
