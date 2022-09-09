3128c15e30ea172ee21d51781de54d84b23d193c
Denzy7 <dennismyner7@gmail.com>
2022-09-09 23:59:19 +0300

    add pulseaudio simple nsl + nocturne.ogg

---
+++ tests/src/testdengine-script/script-nslpulseaudio.c
@@ -0,0 +1,30 @@
+#include <dengine/dengine.h>
+
+int main(int argc, char *argv[])
+{
+    DengineInitOpts* opts = dengine_init_get_opts();
+    opts->window_title = "testdengine-script-nslpulseaudio";
+    dengine_init();
+
+    NSL pa = denginescript_nsl_load("./nsl-pulseaudio.nsl");
+    if(pa == NULL)
+        return 1;
+
+    Script simple;
+    denginescript_nsl_get_script("simple", &simple, pa);
+
+    if(denginescript_call(&simple, DENGINE_SCRIPT_FUNC_START, NULL) != 0)
+    {
+        dengineutils_logging_log("ERROR::Cannot init pulseaudio simple script");
+        return 1;
+    }
+
+    //play whole ogg then exit
+    while(denginescript_call(&simple, DENGINE_SCRIPT_FUNC_UPDATE, NULL) == 0)
+    {
+        dengine_update();
+    }
+    dengine_terminate();
+
+    return 0;
+}
