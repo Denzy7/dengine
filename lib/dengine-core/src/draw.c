2fc966d13a23435cc53987c18fa3b02059198ad6
Denzy7 <dennismyner7@gmail.com>
2023-06-04 16:15:44 +0300

    make entrygl optional for drawing

--- lib/dengine-core/src/draw.c
+++ lib/dengine-core/src/draw.c
@@ -5,6 +5,16 @@
 #include "dengine/buffer.h" //bind
 
 #include "dengine-utils/debug.h"
+
+/* entrygl want needs making of more (unecessary)
+ * calls to driver. use only when embedding */
+int entrygl = 0;
+
+void dengine_draw_enable_entrygl(int state)
+{
+    entrygl = state;
+}
+
 void dengine_draw_primitive(const Primitive* primitive, const Shader* shader)
 {
     DENGINE_DEBUG_ENTER;
@@ -15,13 +25,16 @@ void dengine_draw_primitive(const Primitive* primitive, const Shader* shader)
 
     if(GLAD_GL_VERSION_3_2 || GLAD_GL_ES_VERSION_3_2)
     {
-        dengine_entrygl_vao(&entry_vao );
+        if(entrygl)
+            dengine_entrygl_vao(&entry_vao );
         dengine_vao_bind(&primitive->vao);
     }else
     {
         // get entry stuff
-        dengine_entrygl_buffer(GL_ARRAY_BUFFER, &entry_vbo);
-        dengine_entrygl_buffer(GL_ELEMENT_ARRAY_BUFFER, &entry_ibo );
+        if(entrygl){
+            dengine_entrygl_buffer(GL_ARRAY_BUFFER, &entry_vbo);
+            dengine_entrygl_buffer(GL_ELEMENT_ARRAY_BUFFER, &entry_ibo );
+        }
 
         dengine_buffer_bind(GL_ARRAY_BUFFER, &primitive->array);
         dengine_buffer_bind(GL_ELEMENT_ARRAY_BUFFER, &primitive->index);
@@ -29,22 +42,26 @@ void dengine_draw_primitive(const Primitive* primitive, const Shader* shader)
         //compat needs attribs enabled before draw
         dengine_primitive_attributes_enable(primitive, shader);
     }
+    if(entrygl)
+        dengine_entrygl_shader(&entry_shader );
 
-    dengine_entrygl_shader(&entry_shader );
     dengine_shader_use(shader);
 
     //Don't draw if we have an ERROR. Prevent's infinite thrashing of stdout
     glDrawElements(primitive->draw_mode, primitive->index_count, primitive->draw_type, primitive->offset);
     DENGINE_CHECKGL;
 
-    dengine_shader_use(&entry_shader);
+    if(entrygl)
+    {
+        dengine_shader_use(&entry_shader);
 
-    if(GLAD_GL_VERSION_3_2 || GLAD_GL_ES_VERSION_3_2)
-    {
-        dengine_vao_bind(&entry_vao);
-    }else
-    {
-        dengine_buffer_bind(GL_ARRAY_BUFFER, &entry_vbo);
-        dengine_buffer_bind(GL_ELEMENT_ARRAY_BUFFER, &entry_ibo);
+        if(GLAD_GL_VERSION_3_2 || GLAD_GL_ES_VERSION_3_2)
+        {
+            dengine_vao_bind(&entry_vao);
+        }else
+        {
+            dengine_buffer_bind(GL_ARRAY_BUFFER, &entry_vbo);
+            dengine_buffer_bind(GL_ELEMENT_ARRAY_BUFFER, &entry_ibo);
+        }
     }
 }
