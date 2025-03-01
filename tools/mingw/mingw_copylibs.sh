08a3ec9b78eb90a6533e401f002935d2aa9e9c76
Denzy7 <dennismyner7@gmail.com>
2025-03-01 23:49:22 +0300

    v0.8.1: supplement release for 0.8.0 with fixes
    - fixes some issues that CI wasn't working with github releases
    - install python stblib
    - update readme

--- tools/mingw/mingw_copylibs.sh
+++ tools/mingw/mingw_copylibs.sh
@@ -10,18 +10,29 @@ grepstr="not found|system32|winsxs|linux-vdso|ld-linux"
 sedstr="s/(0x[0-9a-fA-F]*)//g"
 sedstr2="s/.*=> //g"
 
-
 echo "info: p:$prefix v:$ver e:$exe d:$dir x:$xcompile ldd:$ldd"
 
 if [ -n "$ldd" ] && [ "$xcompile" != "TRUE" ]; then
     echo "using system ldd"
-    "$ldd" "$exe" |  grep -Evi "$grepstr" | sed "$sedstr" | sed "$sedstr2" | while read -r file; do
+    # msys2 ldd refuses to cooperate if not ending with dll
+    exe_nsl="$exe"
+    isnsl=0
+    if [[ $exe == *.nsl ]]; then
+        exe_nsl="${exe}.dll"
+        isnsl=1
+        cp -f "${exe}" "${exe_nsl}"
+    fi
+    "$ldd" "$exe_nsl" |  grep -Evi "$grepstr" | sed "$sedstr" | sed "$sedstr2" | while read -r file; do
     if [[ -f "$file" ]]; then
-        cp "$file" "$dir"
+        cp -f "$file" "$dir"
         echo "Copied $file"
     fi
+    # we dont wan't dupes in  outpput
+    if [ "$isnsl" -eq 1 ]; then
+        rm "$exe_nsl"
+    fi
 done
-    exit 0
+#exit 0
 fi
 
 python3 -m venv venv
@@ -39,32 +50,32 @@ fi
 
 "$pip" install pefile
 echo "using mingw-ldd"
-"$python" mingw-ldd/mingw-ldd.py "$exe"
 
-"$python" mingw-ldd/mingw-ldd.py "$exe" | grep -Evi "$grepstr" | sed "$sedstr" | sed "$sedstr2" | while read -r file; do
-    if [[ -f "$file" ]]; then
-        cp "$file" "$dir"
-        echo "Copied $file"
-    fi
+#TODO,WARN,ERROR: --dll-lookup-dirs portable
+lddstr=$("$python" mingw-ldd/mingw_ldd/mingw_ldd.py "$exe" --dll-lookup-dirs /ucrt64/bin)
+echo "${lddstr}"
+echo "${lddstr}" | grep -Evi "$grepstr" | sed "$sedstr" | sed "$sedstr2" | while read -r file; do
+if [[ -f "$file" ]]; then
+    cp -f "$file" "$dir"
+    echo "Copied $file"
+fi
 done
-
-
 
 # special case ntldd depends on libssp
 if [[ -f "$projbindir/ntldd/ntldd.exe" ]];then
     "$python" mingw-ldd/mingw-ldd.py "$projbindir/ntldd/ntldd.exe" | grep -Evi "$grepstr" | sed "$sedstr" | sed "$sedstr2" | while read -r file; do
     if [[ -f "$file" ]]; then
-        cp "$file" "$projbindir/ntldd"
+        cp -f "$file" "$projbindir/ntldd"
     fi
 done
-    echo "using ntldd"
-    "$projbindir/ntldd/ntldd.exe" "$exe" | grep -Evi "$grepstr" | sed "$sedstr" | sed "$sedstr2" | while read -r file; do
-    if [[ -f "$file" ]]; then
-        cp "$file" "$dir"
-        echo "Copied $file"
-    fi
+echo "using ntldd"
+"$projbindir/ntldd/ntldd.exe" "$exe" | grep -Evi "$grepstr" | sed "$sedstr" | sed "$sedstr2" | while read -r file; do
+if [[ -f "$file" ]]; then
+    cp -r "$file" "$dir"
+    echo "Copied $file"
+fi
 done
 
-    "$projbindir/ntldd/ntldd.exe" "$exe"
+"$projbindir/ntldd/ntldd.exe" "$exe"
 fi
 
